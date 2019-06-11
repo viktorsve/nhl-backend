deeab5d3a8ecc19b58a200c0384b47cb79769237
maccydrums <marcusprivat@live.com>
2019-06-11 14:34:09 +0200

    fix

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -1,26 +1,19 @@
-// Mongoose and mocking requests
 const sinon = require('sinon');
-
 const mongoose = require('mongoose')
 mongoose.set('debug', true)
 require('sinon-mongoose')
 
-// initialize the app and models
 const app = require('../../index.js')
 
-// sending requests
 const agent = require('supertest').agent(app);
-// validating results
 const expect = require('chai').expect;
-
-// get the model
 const Account = require('../../models/accounts.js')
 
 var accountMock = sinon.mock(Account)
 
 beforeEach(() => {
 	console.log("beforeEach")
-	accountMock.restore(); // Unwraps the spy
+	accountMock.restore(); 
 });
 
 afterEach( () => {
@@ -53,11 +46,9 @@ describe('account.get', () => {
 			.chain('exec')
 			.resolves([expected]);
 
-			// When (someting happens)
 			agent
 			.get('/accounts')
 			.end((err,res) => {
-			// Then (something should happen)
 				expect(res.status).to.equal(200);
 				expect(res.body).to.eql([expected]);
 				done();
@@ -66,17 +57,16 @@ describe('account.get', () => {
 	
 	it('Should get a user by name', (done) => {
 		accountMock
-		.expects('findOne')
-		.chain('exec')
-		.resolves(expected);
+			.expects('findOne')
+			.chain('exec')
+			.resolves(expected);
 		agent
-		.get('/accounts?name=Marcus')
-		.end((err, res) => {
-			expect(res.status).to.equal(200);
-			expect(res.body).to.eql(expected);
-			done();
+			.get('/accounts?name=Marcus')
+			.end((err, res) => {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.eql(expected);
+				done();
 		})
-
 	})
 });
 });
