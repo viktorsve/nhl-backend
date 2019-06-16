1d8ac4dc376865be92ce06c57c1cd0b222a7eb6f
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2019-06-16 20:44:56 +0200

    Cleaning up routes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const passport = require("passport");
 
 // environment variable PORT or 3000 if unset
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
